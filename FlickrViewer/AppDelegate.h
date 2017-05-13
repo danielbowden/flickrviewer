3004cd33a0c7cc44804b20e606b633cf6b854103
Daniel Bowden <github@bowden.in>
2017-05-13 20:29:28 +1000

    Initial Commit

---
+++ FlickrViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrViewer
+//
+//  Created by Daniel Bowden on 13/5/17.
+//  Copyright © 2017 Daniel Bowden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlickrViewer/AppDelegate.m
@@ -0,0 +1,54 @@
+//
+//  AppDelegate.m
+//  FlickrViewer
+//
+//  Created by Daniel Bowden on 13/5/17.
+//  Copyright © 2017 Daniel Bowden. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    
+    return YES;
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+
+}
+
+@end
