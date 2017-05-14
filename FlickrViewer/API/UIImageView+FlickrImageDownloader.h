e80959eb6464be6a2e8e8130679b3d094ac596ce
Daniel Bowden <github@bowden.in>
2017-05-14 23:58:13 +1000

    Implement flickr image downloading, checking for cached response first

---
+++ FlickrViewer/API/UIImageView+FlickrImageDownloader.h
@@ -0,0 +1,26 @@
+//
+//  UIImageView+FlickrImageDownloader.h
+//  FlickrViewer
+//
+//  Created by Daniel Bowden on 14/5/17.
+//  Copyright © 2017 Daniel Bowden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class Photo;
+
+typedef NS_ENUM(NSInteger, ImageDownloadSize)
+{
+    ImageDownloadSizeThumbnail = 0,
+    ImageDownloadSizeSmall,
+    ImageDownloadSizeMedium,
+    ImageDownloadSizeLarge
+};
+
+@interface UIImageView (FlickrImageDownloader)
+
+- (void)downloadPhoto:(Photo *)photo atSize:(ImageDownloadSize)size placeholderImage:(UIImage *)placeholderImage;
+- (void)downloadPhoto:(Photo *)photo atSize:(ImageDownloadSize)size placeholderImage:(UIImage *)placeholderImage animated:(BOOL)animated;
+
+@end
---
+++ FlickrViewer/API/UIImageView+FlickrImageDownloader.m
@@ -0,0 +1,87 @@
+//
+//  UIImageView+FlickrImageDownloader.m
+//  FlickrViewer
+//
+//  Created by Daniel Bowden on 14/5/17.
+//  Copyright © 2017 Daniel Bowden. All rights reserved.
+//
+
+#import "UIImageView+FlickrImageDownloader.h"
+
+#import "Photo.h"
+
+//Map Flickr size suffixes for appropriate image sizes for this app
+NSString static *const ImageDownloadSizeString[] = {
+    [ImageDownloadSizeThumbnail] = @"m",
+    [ImageDownloadSizeSmall] = @"c",
+    [ImageDownloadSizeMedium] = @"h",
+    [ImageDownloadSizeLarge] = @"k"
+};
+
+NSString static *const kFlickrCDNURL = @"https://staticflickr.com";
+
+@implementation UIImageView (FlickrImageDownloader)
+
+- (void)downloadPhoto:(Photo *)photo atSize:(ImageDownloadSize)size placeholderImage:(UIImage *)placeholderImage
+{
+    [self downloadPhoto:photo atSize:size placeholderImage:placeholderImage animated:NO];
+}
+
+- (void)downloadPhoto:(Photo *)photo atSize:(ImageDownloadSize)size placeholderImage:(UIImage *)placeholderImage animated:(BOOL)animated
+{
+    NSURL *url = [self urlForPhoto:photo size:size];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReturnCacheDataElseLoad timeoutInterval:30.0];
+    NSCachedURLResponse *cachedResponse = [[NSURLCache sharedURLCache] cachedResponseForRequest:request];
+    
+    //Check if we have photo cached first
+    if (cachedResponse.data)
+    {
+            UIImage *image = [UIImage imageWithData:cachedResponse.data];
+            self.image = image;
+    }
+    else if (request)
+    {
+        self.image = placeholderImage;
+        __weak typeof(self)weakSelf = self;
+        NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            
+            if (data)
+            {
+                UIImage *image = [UIImage imageWithData:data];
+                __strong __typeof(weakSelf)strongSelf = weakSelf;
+                if (image)
+                {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        
+                        if (animated)
+                        {
+                            [UIView transitionWithView:self duration:0.4 options:UIViewAnimationOptionTransitionCrossDissolve animations:^{
+                                strongSelf.image = image;
+                            } completion:nil];
+                        }
+                        else
+                        {
+                            strongSelf.image = image;
+                        }
+                    });
+                }
+            }
+        }];
+        
+        [task resume];
+    }
+}
+
+#pragma mark - Private
+
+- (NSURL *)urlForPhoto:(Photo *)photo size:(ImageDownloadSize)size
+{
+    NSURLComponents *components = [NSURLComponents componentsWithString:kFlickrCDNURL];
+    
+    components.host = [NSString stringWithFormat:@"farm%@.%@", photo.farm, components.host];
+    components.path = [NSString stringWithFormat:@"/%@/%@_%@_%@.jpg", photo.server, photo.ID, photo.secret, ImageDownloadSizeString[size]];
+    
+    return components.URL;
+}
+
+@end
