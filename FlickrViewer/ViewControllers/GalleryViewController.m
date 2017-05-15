5b6a849623919ee6266950e00e587640b4fb5d84
Daniel Bowden <github@bowden.in>
2017-05-16 09:24:18 +1000

    Use UISearchController for keyword search

--- FlickrViewer/ViewControllers/GalleryViewController.m
+++ FlickrViewer/ViewControllers/GalleryViewController.m
@@ -14,10 +14,12 @@
 #import "Photo.h"
 #import "GalleryDataSource.h"
 
-@interface GalleryViewController () <UICollectionViewDelegate, CLLocationManagerDelegate>
+@interface GalleryViewController () <UICollectionViewDelegate, CLLocationManagerDelegate, UISearchResultsUpdating>
 
 @property (nonatomic, strong) GalleryDataSource *galleryDataSource;
 @property (nonatomic, strong) CLLocationManager *locationManager;
+@property (nonatomic, strong) UISearchController *searchController;
+@property (nonatomic, strong) NSTimer *searchTimer;
 
 @property (nonatomic, weak) IBOutlet UICollectionView *collectionView;
 @property (nonatomic, weak) IBOutlet UIView *loadingView;
@@ -30,6 +32,8 @@
 - (void)showError:(NSString *)errorMessage;
 - (void)showLoadingWithMessage:(NSString *)message;
 - (void)hideLoading;
+- (void)searchTimerFired;
+- (void)updateSearchRequest:(CLLocation *)location searchTerm:(NSString *)searchTerm;
 
 @end
 
@@ -43,6 +47,13 @@
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
     self.locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers;
+    self.searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
+    self.searchController.searchResultsUpdater = self;
+    self.searchController.searchBar.showsCancelButton = NO;
+    self.searchController.searchBar.tintColor = [UIColor redColor];
+    self.searchController.searchBar.placeholder = @"Keyword search";
+    self.searchController.hidesNavigationBarDuringPresentation = NO;
+    self.searchController.dimsBackgroundDuringPresentation = NO;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -50,6 +61,7 @@
     [super viewWillAppear:animated];
     
     self.collectionView.dataSource = self.galleryDataSource;
+    self.navigationItem.titleView = self.searchController.searchBar;
     
     if ([CLLocationManager locationServicesEnabled] && [CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorizedWhenInUse)
     {
@@ -63,31 +75,24 @@
     }
 }
 
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    if (self.searchTimer)
+    {
+        [self.searchTimer invalidate];
+    }
+}
+
 #pragma mark - LocationManager delegate
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations
 {
     [manager stopUpdatingLocation];
     manager.delegate = nil;
-    
-    [self showLoadingWithMessage:@"Loading photos"];
-    [self.galleryDataSource refreshPhotosWithLocation:locations.lastObject searchTerm:nil completion:^(BOOL success, NSError *error) {
-        
-        __weak typeof(self)weakSelf = self;
-        dispatch_async(dispatch_get_main_queue(), ^{
-            
-            [weakSelf hideLoading];
-            
-            if (success)
-            {
-                [weakSelf.collectionView reloadData];
-            }
-            else
-            {
-                [self showError:error.localizedDescription];
-            }
-        });
-    }];
+
+    [self updateSearchRequest:locations.lastObject searchTerm:self.searchController.searchBar.text];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
@@ -105,6 +110,21 @@
     }
 }
 
+#pragma mark - UISearchResultsUpdating delegate
+
+- (void)updateSearchResultsForSearchController:(UISearchController *)searchController
+{
+    //Debounce the search bar input
+    if (self.searchTimer)
+    {
+        [self.searchTimer invalidate];
+    }
+    
+    if (searchController.searchBar.text.length)
+    {
+        self.searchTimer = [NSTimer scheduledTimerWithTimeInterval:0.8 target:self selector:@selector(searchTimerFired) userInfo:nil repeats:NO];
+    }
+}
 #pragma mark - Button actions
 
 - (IBAction)requestLocationPermission
@@ -131,6 +151,34 @@
 
 #pragma mark - Private methods
 
+- (void)searchTimerFired
+{
+    [self updateSearchRequest:self.locationManager.location searchTerm:self.searchController.searchBar.text];
+}
+
+- (void)updateSearchRequest:(CLLocation *)location searchTerm:(NSString *)searchTerm
+{
+    [self showLoadingWithMessage:@"Loading photos"];
+    [self.galleryDataSource refreshPhotosWithLocation:location searchTerm:searchTerm completion:^(BOOL success, NSError *error) {
+        NSLog(@"refresh response");
+        __weak typeof(self)weakSelf = self;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            
+            [weakSelf hideLoading];
+            
+            if (success)
+            {
+                [weakSelf.collectionView setContentOffset:CGPointZero animated:NO];
+                [weakSelf.collectionView reloadData];
+            }
+            else
+            {
+                [self showError:error.localizedDescription];
+            }
+        });
+    }];
+}
+
 - (void)showLoadingWithMessage:(NSString *)message
 {
     self.locationPermissionView.hidden = YES;
@@ -150,15 +198,18 @@
 
 - (void)showError:(NSString *)errorMessage
 {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Location error" message:errorMessage preferredStyle:UIAlertControllerStyleAlert];
-    [alert addAction:[UIAlertAction actionWithTitle:@"Retry" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-        [self.locationManager startUpdatingLocation];
-    }]];
-    [alert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
-        [self hideLoading];
-    }]];
-    
-    [self presentViewController:alert animated:YES completion:nil];
+    if (!self.presentedViewController)
+    {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Location error" message:errorMessage preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"Retry" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            [self.locationManager startUpdatingLocation];
+        }]];
+        [alert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+            [self hideLoading];
+        }]];
+        
+        [self presentViewController:alert animated:YES completion:nil];
+    }
 }
 
 - (void)didReceiveMemoryWarning
